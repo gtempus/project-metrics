3903a8200e097c6d6742ba30a10d8bfff0a50112
Gary Tempus <gary@heysparkbox.com>
2020-04-01 19:57:07 -0400

    chore: Log request

---
+++ api/controllers/health-check.js
@@ -0,0 +1,35 @@
+module.exports = {
+
+
+  friendlyName: 'Health check',
+
+
+  description: '',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+    success: {
+      responseType: 'view',
+      viewTemplatePath: 'pages/homepage'
+    },
+    notFound: {
+      description: 'No user with the specified ID was found in the database.',
+      responseType: 'notFound'
+    }
+  },
+
+
+  fn: async function (inputs) {
+    console.debug(this.req);
+    // All done.
+    return;
+
+  }
+
+
+};
