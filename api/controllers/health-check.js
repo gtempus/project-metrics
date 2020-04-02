94bdcb454962d60b67d6e32e54ab7e801360796e
Gary Tempus <gary@heysparkbox.com>
2020-04-02 10:15:44 -0400

    fix: bad params for authenticate.

--- api/controllers/health-check.js
+++ api/controllers/health-check.js
@@ -7,10 +7,10 @@ module.exports = {
 
   },
   exits: {
-    success: {
-      responseType: 'view',
-      viewTemplatePath: 'pages/health-check'
-    },
+//    success: {
+//      responseType: 'view',
+//      viewTemplatePath: 'pages/health-check'
+//    },
     notFound: {
       description: 'No user with the specified ID was found in the database.',
       responseType: 'notFound'
@@ -19,9 +19,9 @@ module.exports = {
   fn: async function (inputs) {
     passport.authenticate(
       'bearer',
-      function(req, res) {
-        console.debug(req.user);
-        res.json(req.user);
+      function(err, user, info) {
+        console.debug(user);
+        this.res.json(user);
     });
     // All done.
   return;
